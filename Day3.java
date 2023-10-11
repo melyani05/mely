19d6081ee7f9f426254bef0c78e5d64c20449c44
melyani05 <melyani633@gmail.com>
2023-10-11 19:45:30 +0800

    day3

---
+++ Day3.java
@@ -0,0 +1,15 @@
+
+package day3;
+
+
+public class Day3 {
+
+    
+    public static void main(String[] args) {
+      int nilai1 = 50;
+      int nilai2 = 100;
+      int hasil = nilai1 + nilai2;
+        System.out.println("hasil operasi penjumlahan dari " + hasil);
+    }
+    
+}
