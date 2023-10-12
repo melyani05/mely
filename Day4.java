24beaff929d121a6f21f38facc0b510fce75d456
melyani05 <melyani633@gmail.com>
2023-10-12 18:56:17 +0800

    Day4

---
+++ Day4.java
@@ -0,0 +1,18 @@
+
+package day4;
+
+import java.util.Arrays;
+
+
+public class Day4 {
+
+   
+    public static void main(String[] args) {
+        int[] nilai = {1,2,3,4,5};
+        
+        for (int i=0; 0<nilai.length; i++){
+            System.out.println("element" + i + ":" + Arrays.toString(nilai));
+        }
+    }
+    
+}
