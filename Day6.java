03ab8cb694c22d6ef1e182b93c37374fb07191e6
melyani05 <melyani633@gmail.com>
2023-10-14 22:59:04 +0800

    day6

---
+++ Day6.java
@@ -0,0 +1,15 @@
+
+package day6;
+
+
+public class Day6 {
+
+    
+    public static void main(String[] args) {
+      for (int i = 1; i <= 10; i++) {
+          System.out.println(i);
+      }
+            
+    }
+    
+}
