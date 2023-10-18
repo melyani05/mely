eb126de8acd7841cda2e38899965b18d64c7a24b
melyani05 <melyani633@gmail.com>
2023-10-18 20:06:46 +0800

    day10

---
+++ Day10.java
@@ -0,0 +1,21 @@
+
+package day10;
+import java.util.Scanner;
+
+public class Day10 {
+
+  
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = input.nextInt();
+        if (number > 0) {
+            System.out.println("The number is positive.");
+        } else if (number < 0) {
+            System.out.println("The number is negative.");
+        } else {
+            System.out.println("The number is zero.");
+        }
+    }
+    
+}
