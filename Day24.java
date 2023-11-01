24aa3784297a91fb89c398c7d0f75112c2bdf250
melyani05 <melyani633@gmail.com>
2023-11-01 19:59:08 +0800

    day24

---
+++ Day24.java
@@ -0,0 +1,20 @@
+
+
+package day24;
+
+
+public class Day24 {
+
+    public static void main(String[] args) {
+       int[] angka = {1, 2, 3, 4, 5};
+       
+       for (int i = 0; i < angka.length; i++) {
+            System.out.println(angka[i]);
+        }
+        
+       
+        for (int a : angka) {
+            System.out.println(a);
+        }
+    }
+}
