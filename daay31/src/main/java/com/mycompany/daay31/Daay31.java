1bf669340735da999c32eccb8ef7822b270846d9
melyani05 <melyani633@gmail.com>
2023-11-08 18:54:57 +0800

    day31

---
+++ daay31/src/main/java/com/mycompany/daay31/Daay31.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.daay31;
+
+/**
+ *
+ * @author melia
+ */
+public class Daay31 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
