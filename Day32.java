3cad895c0cd44c8e846a6c6fc3bc4c0f95bf9a0c
melyani05 <melyani633@gmail.com>
2023-11-09 20:00:11 +0800

    day32

---
+++ Day32.java
@@ -0,0 +1,22 @@
+
+
+package com.mycompany.day32;
+
+
+public class Day32 {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+        int c = a + b;
+        int d = a - b;
+        int e = a * b;
+        int f = a / b;
+        int g = a % b;
+        System.out.println("Hasil penjumlahan a dan b adalah " + c);
+        System.out.println("Hasil pengurangan a dan b adalah " + d);
+        System.out.println("Hasil perkalian a dan b adalah " + e);
+        System.out.println("Hasil pembagian a dan b adalah " + f);
+        System.out.println("Sisa pembagian a dan b adalah " + g);
+    }
+}
