f52be6cc54ed09a9eb17e11615bf53446f34b821
melyani05 <melyani633@gmail.com>
2023-11-10 07:26:55 +0800

    day33

---
+++ day33/src/main/java/com/mycompany/day33/Day33.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.day33;
+
+/**
+ *
+ * @author melia
+ */
+public class Day33 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
