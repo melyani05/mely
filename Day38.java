a12dc16b312cf9597423ddfdb22d76a9f75c9fc7
melyani05 <melyani633@gmail.com>
2023-11-15 11:45:08 +0800

    day38

---
+++ Day38.java
@@ -0,0 +1,15 @@
+
+
+package com.mycompany.day38;
+
+
+public class Day38 {
+
+    public static void main(String[] args) {
+      int[] numbers = {1, 2, 3, 4, 5};
+      
+      for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
+}
---
+++ day38/src/main/java/com/mycompany/day38/Day38.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.day38;
+
+/**
+ *
+ * @author melia
+ */
+public class Day38 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
