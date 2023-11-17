099ebf12863b4da17007868433eb2611acede587
melyani05 <melyani633@gmail.com>
2023-11-17 12:06:29 +0800

    day40

---
+++ Day40.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.day40;
+
+
+public class Day40 {
+
+    public static void main(String[] args) {
+         String[] namaTeman = new String[3];
+         
+         namaTeman[0] = "ade";
+         namaTeman[1] = "depi";
+         namaTeman[2] = "chindy";
+         
+         for (String friend : namaTeman) {
+            System.out.println(friend);
+        }
+    }
+}
