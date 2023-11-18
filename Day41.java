bce5211b098caf2682309f5b9c69a71bb1fd6afd
melyani05 <melyani633@gmail.com>
2023-11-18 18:01:07 +0800

    day41

---
+++ Day41.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.day41;
+
+
+public class Day41 {
+
+    public static void main(String[] args) {
+        String[] namaTeman = {"Linda", "Santi", "Susan", "Mila", "Ayu"};
+        
+        
+         System.out.println("Nama teman ke-3: " + namaTeman[2]);
+  }
+    }
+}
---
+++ day41/src/main/java/com/mycompany/day41/Day41.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.day41;
+
+/**
+ *
+ * @author melia
+ */
+public class Day41 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
