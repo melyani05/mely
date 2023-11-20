4dfb7b9c61cd52a0a2cb96ab13a0c402713ab666
melyani05 <melyani633@gmail.com>
2023-11-20 12:23:24 +0800

    day42

---
+++ Day42.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.day42;
+
+
+public class Day42 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        
+         for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
