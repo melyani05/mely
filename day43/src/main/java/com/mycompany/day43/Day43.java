41fa1e5a82deead34f6eaf5ac3878f748ee3135c
melyani05 <melyani633@gmail.com>
2023-11-21 21:31:51 +0800

    day43

---
+++ day43/src/main/java/com/mycompany/day43/Day43.java
@@ -0,0 +1,23 @@
+
+package com.mycompany.day43;
+
+
+public class Day43 {
+
+    public static void main(String[] args) {
+        int[] angka = new int[5];
+
+        angka[0] = 1;
+        angka[1] = 2;
+        angka[2] = 3;
+        angka[3] = 4;
+        angka[4] = 5;
+        
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println(angka[i]);
+        }
+        
+        
+    }
+    
+}
