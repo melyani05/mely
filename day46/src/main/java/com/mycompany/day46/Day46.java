c52b32e94208e5dd3c89c97c0439951c8e4c4683
melyani05 <melyani633@gmail.com>
2023-11-24 22:44:41 +0800

    day46

---
+++ day46/src/main/java/com/mycompany/day46/Day46.java
@@ -0,0 +1,22 @@
+
+package com.mycompany.day46;
+
+
+public class Day46 {
+
+    public static void main(String[] args) {
+        
+        
+        String[] buah = new String[5];
+        
+        buah[0] = "Apel";
+        buah[1] = "Pisang";
+        buah[2] = "Jeruk";
+        buah[3] = "Anggur";
+        buah[4] = "Mangga";
+        
+        for (String b : buah) {
+            System.out.println(b);
+        }
+    }
+}
