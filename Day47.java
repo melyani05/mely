eff4fd7092e09ce272871e653c805303af1161b1
melyani05 <melyani633@gmail.com>
2023-11-25 21:46:04 +0800

    day47

---
+++ Day47.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.day47;
+
+
+public class Day47 {
+
+    public static void main(String[] args) {
+      String[] nama = {"Linda", "Santi", "Susan", "Mila", "Ayu"};
+
+      
+      System.out.println(nama[0]);
+      
+      int panjang = nama.length;
+      
+        System.out.println(panjang);
+    }
+}
