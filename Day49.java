1ff33508032bab6ed3902ef4064f89f1e3592121
melyani05 <melyani633@gmail.com>
2023-11-27 17:25:22 +0800

    day49

---
+++ Day49.java
@@ -0,0 +1,22 @@
+
+
+package com.mycompany.day49;
+
+
+public class Day49 {
+
+    public static void main(String[] args) {
+       int[] anArray;
+       
+       anArray = new int[10];
+       
+       anArray[0] = 100;
+       
+       anArray[1] = 200;
+
+       System.out.println("Element 1 at index 0: " + anArray[0]);
+       System.out.println("Element 2 at index 1: " + anArray[1]);
+        
+        
+    }
+}
