30b9a0e3d68da392fa43121bef7037c986a17f70
melyani05 <melyani633@gmail.com>
2023-11-28 20:04:58 +0800

    day50

---
+++ Day50.java
@@ -0,0 +1,15 @@
+
+
+package com.mycompany.day50;
+
+
+public class Day50 {
+
+    public static void main(String[] args) {
+        String[] buah = {"Apel", "Pisang", "Jeruk"};
+        
+        for (String b : buah) {
+            System.out.println(b);
+        }
+    }
+}
