0d62c966ae4f2930ca10bf0275b22394464d4c4e
melyani05 <melyani633@gmail.com>
2023-11-30 19:55:22 +0800

    day52

---
+++ Day53.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.day53;
+
+
+public class Day53 {
+
+    public static void main(String[] args) {
+        int[] angka = {1, 2, 3, 4, 5};
+
+        
+        System.out.println("Nilai pada indeks 0: " + angka[0]);
+        System.out.println("Nilai pada indeks 2: " + angka[2]);
+    }
+}
