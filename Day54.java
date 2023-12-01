28be2fb1b4f8552d86372ffc1d4a16fa3a022cbf
melyani05 <melyani633@gmail.com>
2023-12-01 11:54:05 +0800

    day54

---
+++ Day54.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.day54;
+
+
+public class Day54 {
+
+    public static void main(String[] args) {
+        int[] myArray = {10, 20, 30, 40, 50};
+        
+        for (int i = 0; i < myArray.length; i++) {
+            System.out.println(myArray[i]);
+        }
+    }
+}
---
+++ day54/src/main/java/com/mycompany/day54/Day54.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.day54;
+
+/**
+ *
+ * @author melia
+ */
+public class Day54 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
