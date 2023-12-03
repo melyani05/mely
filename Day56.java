97e0bb2e4377a486ae0f71756c6142bef9a82de6
melyani05 <melyani633@gmail.com>
2023-12-03 23:06:48 +0800

    day56

---
+++ Day56.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.day56;
+
+
+public class Day56 {
+
+    public static void main(String[] args) {
+      int[] nilai = {80, 70, 90, 85, 65};
+      
+        System.out.println("Elemen ke-3 dari array nilai adalah: " + nilai[2]);
+    }
+}
---
+++ day56/src/main/java/com/mycompany/day56/Day56.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.day56;
+
+/**
+ *
+ * @author melia
+ */
+public class Day56 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
