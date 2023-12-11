462838a47c813885d8889b56d8b67c929c46678d
melyani05 <melyani633@gmail.com>
2023-12-12 00:27:01 +0800

    day63

---
+++ day63/src/main/java/com/mycompany/day63/Day63.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.day63;
+
+
+public class Day63 {
+
+    public static void main(String[] args) {
+         String[] pelanggan = new String[3];
+         
+         pelanggan[0] = "Alice";
+         pelanggan[1] = "Bob";
+         pelanggan[2] = "Charlie";
+         
+         for (String name : pelanggan) {
+            System.out.println(name);
+        }
+    }
+}
