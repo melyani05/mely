e4b9e8a384dfd86d80d165327a9077375a1a3034
melyani05 <melyani633@gmail.com>
2023-12-12 00:00:50 +0800

    day60

---
+++ Day60.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.day60;
+
+
+public class Day60 {
+
+    public static void main(String[] args) {
+        String[] names = {"Alice", "Bob", "Charlie", "David", "Eve"};
+       
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
