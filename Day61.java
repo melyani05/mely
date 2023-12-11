d770ee2a7ff874c0b17441b91dead02a2be5f0bc
melyani05 <melyani633@gmail.com>
2023-12-12 00:07:51 +0800

    day61

---
+++ Day61.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.day61;
+
+
+public class Day61 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        
+         for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
