7d817069401abdf92aab6e6fb6a92aeebd8c1b20
melyani05 <melyani633@gmail.com>
2023-12-12 00:16:19 +0800

    day62

---
+++ Day62.java
@@ -0,0 +1,15 @@
+
+package com.mycompany.day62;
+
+
+public class Day62 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+
+    }
+}
---
+++ day62/src/main/java/com/mycompany/day62/Day62.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.day62;
+
+/**
+ *
+ * @author melia
+ */
+public class Day62 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
