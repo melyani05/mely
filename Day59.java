6026ad88851b06854af8bdd1107c72c7558780ea
melyani05 <melyani633@gmail.com>
2023-12-11 23:50:02 +0800

    day59

---
+++ Day59.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.day59;
+
+
+public class Day59 {
+
+    public static void main(String[] args) {
+        int[] numbers = new int[5];
+        
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        numbers[3] = 4;
+        numbers[4] = 5;
+        
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+}
