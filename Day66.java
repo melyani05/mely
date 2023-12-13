857de26583892f4bac55d10266746ee4d9a95ee3
melyani05 <melyani633@gmail.com>
2023-12-13 21:37:41 +0800

    day66

---
+++ Day66.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.day66;
+
+
+public class Day66 {
+
+    public static void main(String[] args) {
+        String nama = "Melly";
+        int umur = 19;
+        float nilai = 9.5f;
+        char a = 'a';
+        boolean apakahBenar = true; // false
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur);
+        System.out.println("Nilai: " + nilai);
+        System.out.println("Karakter: " + a);
+        System.out.println("Apakah Benar: " + apakahBenar);
+    }
+}
---
+++ day66/src/main/java/com/mycompany/day66/Day66.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.day66;
+
+/**
+ *
+ * @author melia
+ */
+public class Day66 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
