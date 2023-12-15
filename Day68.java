fc3e767d5419be25f59597a8a4f5f21063d3c5a2
melyani05 <melyani633@gmail.com>
2023-12-15 08:34:38 +0800

    day68

---
+++ Day68.java
@@ -0,0 +1,20 @@
+
+package com.mycompany.day68;
+
+
+public class Day68 {
+
+    public static void main(String[] args) {
+        String nama = "John Doe";
+        int umur = 20;
+        float nilai = 9.5f;
+        char a = 'a';
+        boolean apakahBenar = true;
+
+        System.out.println("Tipe Data String: " + nama.getClass());
+        System.out.println("Tipe Data Integer: " + umur.getClass());
+        System.out.println("Tipe Data Float: " + nilai.getClass());
+        System.out.println("Tipe Data Character: " + a.getClass());
+        System.out.println("Tipe Data Boolean: " + apakahBenar.getClass());  
+    }
+}
