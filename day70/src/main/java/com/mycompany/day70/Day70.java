39b24523b2b8fc55dbc25266f011b2a3f04422df
melyani05 <melyani633@gmail.com>
2023-12-17 22:44:09 +0800

    day70

---
+++ day70/src/main/java/com/mycompany/day70/Day70.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.day70;
+
+/**
+ *
+ * @author melia
+ */
+public class Day70 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
