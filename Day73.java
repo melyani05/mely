26514421e283d4fa6dc20deb61b7f55153013aa2
melyani05 <melyani633@gmail.com>
2023-12-21 00:19:20 +0800

    day73

---
+++ Day73.java
@@ -0,0 +1,17 @@
+
+package com.mycompany.day73;
+
+import java.util.Scanner;
+public class Day73 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double angka1, angka2, hasil;
+        System.out.println("Masukkan angka pertama: ");
+        angka1 = input.nextDouble();
+        System.out.println("Masukkan angka kedua: ");
+        angka2 = input.nextDouble();
+        hasil = angka1 + angka2; // Ganti dengan operasi matematika yang diinginkan
+        System.out.println("Hasil: " + hasil);
+    }
+}
