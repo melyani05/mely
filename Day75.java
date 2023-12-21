07ab9f9b9649cdc719ffb8a33b5064d58b6f2974
melyani05 <melyani633@gmail.com>
2023-12-22 00:13:45 +0800

    day75

---
+++ Day75.java
@@ -0,0 +1,21 @@
+
+
+package com.mycompany.day75;
+
+import java.util.Stack;
+
+public class Day75 {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println("Elements popped from the stack:");
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
+}
