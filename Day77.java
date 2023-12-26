407871193ef0c80089e73e590281f34f592ccbff
melyani05 <melyani633@gmail.com>
2023-12-26 11:17:09 +0800

    day77

---
+++ Day77.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.day77;
+
+import java.util.Stack;
+public class Day77 {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack);
+    }
+}
