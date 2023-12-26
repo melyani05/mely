e2b818997f4552c2ca2ac95387778a2f60d5249c
melyani05 <melyani633@gmail.com>
2023-12-26 11:22:08 +0800

    day78

---
+++ Day78.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.day78;
+
+import java.util.Stack;
+
+public class Day78 {
+
+    public static void main(String[] args) {
+       Stack<Integer> stack = new Stack<>();
+       
+       stack.push(1);
+       stack.push(2);
+       stack.push(3);
+       stack.pop();
+       System.out.println(stack);
+   
+    }
+}
