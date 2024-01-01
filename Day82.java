4035e9c7bec1bb9b49c458f282a0ae21bc4d1d9f
melyani05 <melyani633@gmail.com>
2024-01-01 19:19:06 +0800

    day82

---
+++ Day82.java
@@ -0,0 +1,24 @@
+
+
+package com.mycompany.day82;
+
+import java.util.Stack;
+
+public class Day82 {
+
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.println(stack.empty());Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(2);
+        stack.push(3);
+        System.out.println(stack.pop());
+        System.out.println(stack.peek());
+        System.out.println(stack.empty());
+    }
+}
