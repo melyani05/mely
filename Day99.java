2b403fc5b64f61878e51d4125cec3953827f2b06
melyani05 <melyani633@gmail.com>
2024-01-15 22:54:49 +0800

    day99

---
+++ Day99.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.day99;
+
+
+public class Day99 {
+
+    public ,llkstatic void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        // Menjumlahkan elemen array menggunakan perulangan for
+        int sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+
+        System.out.println("Jumlah elemen array: " + sum);
+    }
+}
---
+++ day99/src/main/java/com/mycompany/day99/Day99.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.day99;
+
+/**
+ *
+ * @author melia
+ */
+public class Day99 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
