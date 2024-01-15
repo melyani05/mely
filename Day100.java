9c5b62c99fab8fc25c8198a85d31b4f3b739450d
melyani05 <melyani633@gmail.com>
2024-01-15 23:01:57 +0800

    day100

---
+++ Day100.java
@@ -0,0 +1,16 @@
+
+package com.mycompany.day100;
+
+
+public class Day100 {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        // Mencetak elemen array menggunakan perulangan for
+        System.out.println("Elemen array:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
